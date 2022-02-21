2f055a742c488cc27ebd964346922a67056b55a0
dmitry.chernetsov <dmitry.chernetsov@ntlab.com>
2022-02-21 19:29:24 +0300

    added countdown timer

--- food/js/main.js
+++ food/js/main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
+    //Tabs
     const tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector(".tabheader__items");
@@ -43,4 +45,57 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 
+    //Timer
+    let endTime = '2022-03-08';
+
+    function getTimeRemaning(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    //для красоты (добавления 0 к одному числу)
+    function setZero(num){
+        if (num>=0 && num <10){
+            return `0${num}`;
+        }else{
+            return num;
+        }
+    }
+
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();// чтобы не было задержики в 1сек
+
+        function updateClock() {
+            const t = getTimeRemaning(endTime);
+
+            days.innerHTML = setZero(t.days);
+            hours.innerHTML = setZero(t.hours);
+            minutes.innerHTML = setZero(t.minutes);
+            seconds.innerHTML = setZero(t.seconds);
+
+            if (t.total <=0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', endTime);
 });
