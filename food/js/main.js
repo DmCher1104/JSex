d173017fe52c57472932c4c7b6eaf858b3f847c3
dmitry.chernetsov <dmitry.chernetsov@ntlab.com>
2022-03-09 19:31:04 +0300

    added json-server with npm

--- food/js/main.js
+++ food/js/main.js
@@ -334,4 +334,8 @@ document.addEventListener('DOMContentLoaded', function () {
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res=>console.log(res));
 });
